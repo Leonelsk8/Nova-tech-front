0e4c1deb9f4292cc2b76fb41e5e28768604a7000
carlospereyra246 <carlosppereyra@gmail.com>
2023-04-26 23:48:36 -0300

    añadiendo contacto cp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
-
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Contact />
     </div>
   )
 }
