4e071b2e7ef426fcc1ad6e3d45f7902d27f59502
Matias95Rosales <r.matiasssexe@gmail.com>
2023-04-29 18:05:23 -0300

    login-conSusCarpetas-mr

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import en from './English.json';
-import es from './Spanish.json';
+import {Routes,Route} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/login/Login';
+// // import en from './English.json';
+// // import es from './Spanish.json';
+// // ---------
+// // function App() {
+
+// //   const {idiom, setIdiom} = useState(es);
+
+// //   const idiomChange = (value)=>{
+// //     value === 'es' ? setIdiom(en) : setIdiom(es);
+// //   }
+
+// //   return (
+// //     <>
+      
+// //     </>
+// //   )
+// // }
+// // ------------
 
 function App() {
-
-  const {idiom, setIdiom} = useState(es);
-
-  const idiomChange = (value)=>{
-    value === 'es' ? setIdiom(en) : setIdiom(es);
-  }
-
   return (
     <>
-      
+    <Navbar/>
+    <Routes>
+      <Route path="/login"element={<Login />}/>
+    </Routes>
     </>
   )
 }
 
 export default App
+
