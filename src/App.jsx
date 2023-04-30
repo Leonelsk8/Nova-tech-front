53820905d57a48dedab3bd210cec7d720b7e5f02
carlospereyra246 <carlosppereyra@gmail.com>
2023-04-30 13:37:35 -0300

    subiendo cambios en aboutus cp mr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AboutUs from './components/page/AboutUs';
 import Contact from './components/contact/Contact';
 import Products from './components/page/Products';
 import Home from './components/page/Home';
@@ -18,11 +17,10 @@ function App() {
       <Routes>
         <Route path='/' element={< Home/>} />
         <Route path='contact' element={<Contact />} />
-        <Route path='aboutUS' element={<AboutUs />} />
         <Route path='contact' element={<Contact />} />
         <Route path='products' element={<Products />} />
         <Route path='*' element={< NotFound/>} />
-        <Route path='abautUs' element={< AboutUS/>} />
+        <Route path='aboutUs' element={< AboutUS/>} />
       </Routes>
     </>
   );
--- src/components/aboutUS/AboutUS.jsx
+++ src/components/aboutUS/AboutUS.jsx
@@ -1,50 +1,48 @@
 import './AboutUs.css';
+
 const AboutUS = () => {
-  let message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, debitis eligendi rerum hic ducimus culpa quidem aperiam in nam sed eius, incidunt deserunt inventore suscipit accusantium`;
+  let message = `Somos un gran equipo bien especializados, siempre trabajando y pensando en ud en nuetra gente, nuestro publico.`;
   return (
     <section className='section-white'>
       <div className='container'>
         <div className='row justify-content-between'>
           <div className='col-md-12 text-center'>
             <h2 className='section-title'></h2>
-            the team behind pacifico
+            Siempre Pensando en ud equipo Nova Tech
           </div>
           <p className='section-subtitle'>{message}</p>
-
           <div className='col-sm-4 col-md-3'>
             <di className='card team-item'>
               <div className='text-center m-2'>
                 <img src='/img/Leo.png' className='team-img' alt='imagen' />
               </div>
               <h3>Leonel Gomez</h3>
-              <div className='team-info'></div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quibusdam, debitis eligendi rerum hic ducimus culpa quidem
-                aperiam in nam sed eius, incidunt deserunt inventore suscipit
-                accusantium
-              </p>
-
-              <ul className='icon'>
-                <div>
-                  <a href='#' className='twitter'>
-                  <i className="bi bi-twitter"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='whatsapp'>
-                  <i className="bi bi-whatsapp"></i>
-
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='facebook'>
-                  <i className="bi bi-facebook"></i>
-                  </a>
-                </div>
-              </ul>
+              <div className='team-info'>
+                <p>
+                  Edad: 22 Años Estudiante: Desarrollo Web (rolling Code)
+                  Estudiante de ingeniería en sistemas de información, razón que
+                  me motivó a hacer Code school. Y ahora mi mayor pasatiempo es
+                  programar porque es algo que me gusta. Tengo bases de
+                  programación gracias a la carrera de Ingeniería.
+                </p>
+                <ul className='icon'>
+                  <div>
+                    <a href='#' className='twitter'>
+                      <i className='bi bi-twitter'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='whatsapp'>
+                      <i className='bi bi-whatsapp'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='facebook'>
+                      <i className='bi bi-facebook'></i>
+                    </a>
+                  </div>
+                </ul>
+              </div>
             </di>
           </div>
           <div className='col-sm-4 col-md-3'>
@@ -53,33 +51,32 @@ const AboutUS = () => {
                 <img src='/img/Ivan.png' className='team-img' alt='imagen' />
               </div>
               <h3>Ivan Economo</h3>
-              <div className='team-info'></div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quibusdam, debitis eligendi rerum hic ducimus culpa quidem
-                aperiam in nam sed eius, incidunt deserunt inventore suscipit
-                accusantium
-              </p>
-
-              <ul className='icon'>
-                <div>
-                  <a href='#' className='twitter'>
-                  <i className="bi bi-twitter"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='whatsapp'>
-                  <i className="bi bi-whatsapp"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='facebook'>
-                  <i className="bi bi-facebook"></i>
-                  </a>
-                </div>
-              </ul>
+              <div className='team-info'>
+                <p>
+                  Edad: 27 Años Estudiante: Desarrollo Web (rolling Code)
+                  Empezando mí camino como desarrollador. Mi objetivo es
+                  especializarme y conocer a fondo las herramientas de
+                  desarrollo para poder crear estructuras complejas de manera
+                  profunda y profesional.
+                </p>
+                <ul className='icon'>
+                  <div>
+                    <a href='#' className='twitter'>
+                      <i className='bi bi-twitter'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='whatsapp'>
+                      <i className='bi bi-whatsapp'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='facebook'>
+                      <i className='bi bi-facebook'></i>
+                    </a>
+                  </div>
+                </ul>
+              </div>
             </div>
           </div>
           <div className='col-sm-4 col-md-3'>
@@ -88,33 +85,31 @@ const AboutUS = () => {
                 <img src='/img/Matias.png' className='team-img' alt='imagen' />
               </div>
               <h3>Matias Rosales</h3>
-              <div className='team-info'></div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quibusdam, debitis eligendi rerum hic ducimus culpa quidem
-                aperiam in nam sed eius, incidunt deserunt inventore suscipit
-                accusantium
-              </p>
-
-              <ul className='icon'>
-                <div>
-                  <a href='#' className='twitter'>
-                  <i className="bi bi-twitter"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='whatsapp'>
-                  <i className="bi bi-whatsapp"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='facebook'>
-                  <i className="bi bi-facebook"></i>
-                  </a>
-                </div>
-              </ul>
+              <div className='team-info'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quibusdam, debitis eligendi rerum hic ducimus culpa quidem
+                  aperiam in nam sed eius, incidunt deserunt inventore suscipit
+                  accusantium
+                </p>
+                <ul className='icon'>
+                  <div>
+                    <a href='#' className='twitter'>
+                      <i className='bi bi-twitter'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='whatsapp'>
+                      <i className='bi bi-whatsapp'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='facebook'>
+                      <i className='bi bi-facebook'></i>
+                    </a>
+                  </div>
+                </ul>
+              </div>
             </div>
           </div>
           <div className='col-sm-4 col-md-3'>
@@ -123,33 +118,31 @@ const AboutUS = () => {
                 <img src='/img/Carlos.png' className='team-img' alt='imagen' />
               </div>
               <h3>Pereyra Carlos</h3>
-              <div className='team-info'></div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quibusdam, debitis eligendi rerum hic ducimus culpa quidem
-                aperiam in nam sed eius, incidunt deserunt inventore suscipit
-                accusantium{' '}
-              </p>
-
-              <ul className='icon'>
-                <div>
-                  <a href='#' className='twitter'>
-                  <i className="bi bi-twitter"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='whatsapp'>
-                  <i className="bi bi-whatsapp"></i>
-                  </a>
-                </div>
-
-                <div>
-                  <a href='#' className='facebook'>
-                  <i className="bi bi-facebook"></i>
-                  </a>
-                </div>
-              </ul>
+              <div className='team-info'>
+                <p>
+                  Edad: 34 Años Estudiante: Desarrollo Web (rolling Code)
+                  Actualmente trabajando de seguridad privada, no tengo definido
+                  que hacer al finalizar el cursado, pero con grandes
+                  expectátivas de cambiar mi futuro.
+                </p>
+                <ul className='icon'>
+                  <div>
+                    <a href='#' className='twitter'>
+                      <i className='bi bi-twitter'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='whatsapp'>
+                      <i className='bi bi-whatsapp'></i>
+                    </a>
+                  </div>
+                  <div>
+                    <a href='#' className='facebook'>
+                      <i className='bi bi-facebook'></i>
+                    </a>
+                  </div>
+                </ul>
+              </div>
             </di>
           </div>
         </div>
