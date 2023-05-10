3868e10c93c2b6dfd822e4cf5092b4e0192cc402
carlospereyra246 <carlosppereyra@gmail.com>
2023-05-09 21:52:36 -0300

    subiendo cambios en contacto

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,4 +1,4 @@
-import { Container, Col, Row } from 'react-bootstrap';
+import{ Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import style from './NotFound.module.css';
 
