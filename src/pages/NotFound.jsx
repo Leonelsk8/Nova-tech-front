6ae160715636aadbdfa44deb6392dc5965fe9b18
Leonelsk8 <leonelgomez.sk8@gmail>
2023-05-10 20:24:33 -0300

    agregando cambios

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,18 @@
-import { Container, Col, Row } from 'react-bootstrap';
+import{ Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import style from './NotFound.module.css';
 
-const NotFound = () => {
+const NotFound = (props) => {
+  const { modeDL, textDL, lang } = props;
   return (
-    <div className>
-      <Container className>
-        <Row className='mt-5'>
+    <div className={`${style.body} vh-100 d-flex justify-content-center align-items-center`}>
+      <Container className='md-12 '>
+        <Row>
           <Col md={{ span: 6, offset: 3 }} className='text-center'>
-            <img className={style.img404} src='/img/error.png' alt='error 404' />
-            <h2> estas en el camino Incorrecto</h2>
-            <p>
-              Regresa al <Link to='/home'>Inicio</Link>
+            <img className={`${style.img404}`} src='/img/error.png' alt='error 404' />
+            <h2 className={`${style.error}`} > Estas en el camino Incorrecto</h2>
+            <p className={`${style.error}`}>
+              Regresa al <Link to='Routes.Home' className={`${style.inicio}`}>Inicio</Link>
             </p>
           </Col>
         </Row>
